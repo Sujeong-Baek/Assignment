abf6b558b8c98b16ed8f545632c46fe50fb63e7a
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-02-13 13:59:40 +0900

    [update] main()에 타일2048생성시 게임완료조건 추가

--- game2048.py
+++ game2048.py
@@ -216,6 +216,13 @@ def main():
             points+=point
             if moved:
                 board.insert()
+        for i in range(4):
+            for j in range(4):
+                if 2048 == board.nums[i][j]:
+                    print(board)
+                    print("\nYou win!!!")
+                    print(f"You have {points} points.")
+                    return
         if board.is_full():
             print(board)
             print("\nGame over.")
@@ -232,6 +239,3 @@ def main():
 # [y][x]
 # (x, y)
 # row, column
-
-
-main()
