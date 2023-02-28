398f63c25e1c340881374aa5a34faa86fd7bccff
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-02-28 13:47:03 +0900

    [update] reverse_string수정했습니다.

--- algorithms/reverse_string.py
+++ algorithms/reverse_string.py
@@ -1,5 +1,8 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120822
 
+
 def solution(my_string):
-    answer = ''
-    return answer
+    if len(my_string)==1:
+        return my_string[-1]
+    return my_string[-1]+solution(my_string[:-1])
+
