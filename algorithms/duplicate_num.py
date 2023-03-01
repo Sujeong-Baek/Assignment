6316228905b1000b24e7cb09e11a9e06a0596fd8
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-01 20:48:51 +0900

    [update] 수정했습니다.

--- algorithms/duplicate_num.py
+++ algorithms/duplicate_num.py
@@ -1,8 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120583
 
 def solution(array, n):
+    # return sum([num == n for num in array])
     answer = 0
     for num in array:
         if num == n:
             answer += 1
     return answer
+
+# list comprehension
+
+print([i for i in range(4)])
+print([num == 2 for num in [1,2,3,4]])
