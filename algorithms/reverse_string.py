bc96a3514d95e5fd321782694c070eca2f8f2571
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-01 20:38:22 +0900

    [update] reverse수정했습니다.

--- algorithms/reverse_string.py
+++ algorithms/reverse_string.py
@@ -6,3 +6,14 @@ def solution(my_string):
         return my_string[-1]
     return my_string[-1]+solution(my_string[:-1])
 
+# recursive vs iterative
+
+# 'abcd'
+# d -> c -> b -> a
+def solution2(my_string):
+    answer=''
+    for idx in range(len(my_string)-1, -1, -1):
+        answer+=my_string[idx]
+    return answer
+
+print(solution2("abcd"))
