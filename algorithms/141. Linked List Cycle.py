1660ba4a92858268d032c31e4f8c1c39991115c7
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-17 12:17:10 +0900

    [update]

--- algorithms/141. Linked List Cycle.py
+++ algorithms/141. Linked List Cycle.py
@@ -5,4 +5,11 @@ class ListNode:
         self.next = None
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
+    def hasCycle(self, head: ListNode) -> bool:
+        visited=set()
+        while head:
+            if head in visited:
+                return True
+            visited.add(head)
+            head=head.next
+        return False
