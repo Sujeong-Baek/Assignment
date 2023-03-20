61c15c687c361471ac6c484fad4e3291109b604e
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-21 02:50:24 +0900

    [update]

--- algorithms/876. Middle of the Linked List.py
+++ algorithms/876. Middle of the Linked List.py
@@ -6,3 +6,14 @@ class ListNode:
         self.next = next
 class Solution:
     def middleNode(self, head: ListNode) -> ListNode:
+        p=head
+        lenth=0        
+        while p:
+            p=p.next
+            lenth+=1
+
+        middle=lenth//2
+        while middle>0:
+            head=head.next
+            middle -=1
+        return head
