7728ad7e3657982b3f222e1d79c27c58eb97fbb2
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-21 03:05:57 +0900

    숙제추가

---
+++ algorithms/1290. Convert Binary Number.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
