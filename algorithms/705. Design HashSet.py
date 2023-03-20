a95fae0c8789302d364f18725ad807402807c034
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-20 22:23:32 +0900

    [update]

--- algorithms/705. Design HashSet.py
+++ algorithms/705. Design HashSet.py
@@ -1,15 +1,21 @@
 class MyHashSet:
 
     def __init__(self):
-         
+        self.hashSet=[]
+  
 
     def add(self, key: int) -> None:
-
+        if not key in self.hashSet:
+            self.hashSet.append(key)
 
     def remove(self, key: int) -> None:
-                   
+        if key in self.hashSet: 
+            self.hashSet.remove(key)                      
 
     def contains(self, key: int) -> bool:
+        if key in self.hashSet:
+            return True
+        return False
 
 
 # Your MyHashSet object will be instantiated and called as such:
