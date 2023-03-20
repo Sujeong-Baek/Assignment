4bb276fe118d38037ebadf0c42ab6bae7c8a1c53
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-21 03:08:21 +0900

    [update]

--- algorithms/1290. Convert Binary Number.py
+++ algorithms/1290. Convert Binary Number.py
@@ -6,3 +6,18 @@ class ListNode:
         self.next = next
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
+        p=head
+        length=0
+        answer=0
+
+        while p:
+            p=p.next
+            length+=1
+        
+        exponent=length-1
+        while exponent>=0:
+            if head.val:
+                answer+=2**exponent
+            head=head.next
+            exponent-=1
+        return answer
