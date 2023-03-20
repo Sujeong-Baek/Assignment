eb636c29cabff00bdcf173abe9cce131fe0e1e12
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-21 02:22:56 +0900

    [update]

--- algorithms/706. Design HashMap.py
+++ algorithms/706. Design HashMap.py
@@ -2,16 +2,26 @@
 class MyHashMap:
 
     def __init__(self):
-
+        self.hashmap=[]
 
     def put(self, key: int, value: int) -> None:  
-
+        mapping=[key,value]
+        for i in range(len(self.hashmap)):
+            if self.hashmap[i][0]==key:
+                self.hashmap[i]=mapping
+                return
+        self.hashmap.append(mapping)
 
     def get(self, key: int) -> int:
-
+        for mapping in self.hashmap:
+            if key == mapping[0]:
+                return mapping[-1]
+        return -1
 
     def remove(self, key: int) -> None:
-
+        for mapping in self.hashmap:
+            if key ==mapping[0]:
+                self.hashmap.remove(mapping)
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
