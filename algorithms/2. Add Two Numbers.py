50778c3e1f8909e4fe7f3415585bba10e3fbb7cf
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-21 17:24:28 +0900

    [update]

--- algorithms/2. Add Two Numbers.py
+++ algorithms/2. Add Two Numbers.py
@@ -6,3 +6,33 @@ class ListNode:
         self.next = next
 class Solution:
     def addTwoNumbers(self, l1:ListNode, l2: ListNode) ->ListNode:
+        nextnum=0
+        answer=ListNode()
+        p=answer
+   
+        while l1 or l2:
+            addnum=0
+            num=0            
+            if l1 and l1.val:
+                addnum+=l1.val
+            if l1:
+                l1=l1.next
+
+            if l2 and l2.val:
+                addnum+=l2.val
+            if l2:
+                l2=l2.next
+            
+            num+=(addnum+nextnum)            
+            p.next=ListNode(val=num%10)
+            p=p.next
+           
+            if num>=10:
+                nextnum=1
+            else:
+                nextnum=0
+                
+        if nextnum:
+            p.next=ListNode(val=1)
+
+        return answer.next
