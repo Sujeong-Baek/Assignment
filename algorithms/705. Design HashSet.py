abfe558ed34aba8c5274307cd47a26b77f2c1244
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-25 21:31:32 +0900

    수업완료

--- algorithms/705. Design HashSet.py
+++ algorithms/705. Design HashSet.py
@@ -1,26 +1,19 @@
-# https://leetcode.com/problems/design-hashset/
-class MyHashSet:
-
+class Mydata:
     def __init__(self):
-        self.hashSet=[]
+        self.data=[]
   
 
     def add(self, key: int) -> None:
-        if not key in self.hashSet:
-            self.hashSet.append(key)
+        if not key in self.data:
+            self.data.append(key)
 
     def remove(self, key: int) -> None:
-        if key in self.hashSet: 
-            self.hashSet.remove(key)                      
+        for i, k in enumerate(self.data):
+            if k == key:
+                self.data.pop(i)
 
     def contains(self, key: int) -> bool:
-        if key in self.hashSet:
-            return True
-        return False
+        return key in self.data
 
-
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+lst = [1,2,3,4]
+# lst.pop(2) >> 3
