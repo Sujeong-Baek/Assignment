5db496133c32e4292e364f3aba6a78b90bba2a0c
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-25 21:03:33 +0900

    수업완료

--- algorithms/19. Remove Nth Node From End.py
+++ algorithms/19. Remove Nth Node From End.py
@@ -6,19 +6,20 @@ class ListNode:
         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        length=0
-        p=head
-        sentinel=q=ListNode()
-        q.next=head
-        while p:
-            length+=1
-            p=p.next
+        p=q=head
+        for _ in range(n):
+            q=q.next #q=3
+        if not q:
+            return head.next
+        while q.next:
+            p=p.next #14  25 36
+            q=q.next
+        p.next=p.next.next  #4 =6
+        return head
 
-        count=length-n
-        while q and q.next:
-            if count != 0:    
-                q=q.next                
-            else:
-                q.next=q.next.next                        
-            count-=1
-        return sentinel.next
+
+# 1,2,3,4,5, 6 // 3
+#     p      q
+# 1,2,3,5,6
+
+# 1 // 1
