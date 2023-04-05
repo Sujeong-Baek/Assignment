56801f1f487045cd3ec0d782c5a63dbef8cbedda
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-06 01:42:17 +0900

    [update]

--- programmers/0.ox퀴즈.py
+++ programmers/0.ox퀴즈.py
@@ -1,1 +1,19 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120907
+def solution(quiz):
+    answer = []
+    #quiz 배열 문자열 하나씩 쪼개기 
+    #if구절로 +,-연산 =뒤에오는 숫자와 정답 비교하기
+    #맞으면o 아니면 x
+    for q in quiz:
+        q_list=q.split()        
+        if q_list[1] =="+":
+            ans=int(q_list[0])+int(q_list[2])
+        elif q_list[1] =="-":
+            ans=int(q_list[0])-int(q_list[2])
+            
+        if ans==int(q_list[-1]):
+            answer+="O"
+        else:
+            answer+="X"
+        
+    return answer
