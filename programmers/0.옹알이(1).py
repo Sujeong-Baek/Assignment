ff750f7fc3d5eed6918c61e33f8250e220349dbc
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-05 16:56:22 +0900

    [update]

--- programmers/0.옹알이(1).py
+++ programmers/0.옹알이(1).py
@@ -2,4 +2,15 @@
 def solution(babbling):
     answer = 0
 
+    baby_list=["aya", "ye", "woo", "ma"]
+    talk_list=[]
+    
+    for bab in babbling:
+        for baby in baby_list:
+            bab=bab.replace(baby,"1")
+        talk_list.append(bab)
+    
+    for talk in talk_list:
+        if talk.isdigit():
+            answer+=1
     return answer
