d1c9f47ee208b1de7c47ac8cc01c0833a6ca09d2
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-06 01:25:15 +0900

    [update]

--- programmers/0.최빈값구하기.py
+++ programmers/0.최빈값구하기.py
@@ -1,1 +1,18 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120812
+def solution(array):
+    a_map={}
+    answer=[]
+    for a in array:
+        if not a in a_map:
+            a_map[a]=1
+        else:
+            a_map[a]+=1
+
+    max_a=max(a_map.values())
+    for key in a_map:
+        if a_map[key]==max_a:
+            answer.append(key)
+
+    if len(answer)>1:
+        return -1
+    return answer[0]
