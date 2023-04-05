383d0f7b6fcb2bfd29625382cdcb559f62a94dca
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-06 00:37:51 +0900

    [udpate] prefix

--- programmers/0.옹알이(1).py
+++ programmers/0.옹알이(1).py
@@ -2,15 +2,23 @@
 def solution(babbling):
     answer = 0
 
-    baby_list=["aya", "ye", "woo", "ma"]
-    talk_list=[]
+    babies=["aya", "ye", "woo", "ma"]
     
     for bab in babbling:
-        for baby in baby_list:
-            bab=bab.replace(baby,"1")
-        talk_list.append(bab)
-    
-    for talk in talk_list:
-        if talk.isdigit():
-            answer+=1
-    return answer
+        found = True
+        while bab and found:
+            found = False
+            for baby in babies:
+                if baby==bab[:len(baby)]:
+                    bab=bab[len(baby):]
+                    found = True
+        if not bab:
+            answer += 1
+    return answer         
+
+# bayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaaya
+# ayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaaya
+# ayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaayaaya
+
+def prefix(a, b):
+    return a == b[:len(a)]
