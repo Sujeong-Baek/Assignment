735bfe9606fbe11c95985b5836247549a1ca2eca
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-06 01:03:47 +0900

    [update]

--- programmers/0.영어가싫어요.py
+++ programmers/0.영어가싫어요.py
@@ -1,1 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120894
+def solution(numbers):
+    number=[ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+    for i, n in enumerate(number):
+        numbers=numbers.replace(str(n),str(i))
+    return int(numbers)
