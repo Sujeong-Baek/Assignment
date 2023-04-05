8a03fd0583577a836090aa09a19e21da6e7908a3
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-06 01:33:09 +0900

    [update]

--- programmers/0.컨트롤제트.py
+++ programmers/0.컨트롤제트.py
@@ -1,1 +1,11 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120853
+
+def solution(s):
+    s_list = s.split()
+    answer=0
+    for i in range(len(s_list)):
+        if s_list[i]=='Z':
+            answer-=int(s_list[i-1])
+        else:
+            answer+=int(s_list[i])
+    return answer
