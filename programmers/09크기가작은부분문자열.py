00563b32506d59dff558d270809fb447068fe5ff
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-06 22:50:58 +0900

    [update]

--- programmers/09크기가작은부분문자열.py
+++ programmers/09크기가작은부분문자열.py
@@ -1,1 +1,12 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/147355
+def solution(t, p):
+    
+    count=0
+    len_t=len(t)
+    len_p=len(p)
+    
+    for i in range(len_t):
+        if i+len_p<=len_t:
+            if int(t[i:i+len_p]) <=int(p):
+                count+=1
+    return count
