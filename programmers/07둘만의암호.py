aa7c8feb9f098548ce5d6915a07798ddf4e86693
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-06 19:33:17 +0900

    [update]

--- programmers/07둘만의암호.py
+++ programmers/07둘만의암호.py
@@ -1,1 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/155652
+def solution(s, skip, index):
+    answer=''
+    for el in s:        
+        count=index        
+        while count>0:          
+            if ord(el)+1>122:                
+                el=chr(ord(el)+1-26)
+            else:   
+                el=chr(ord(el)+1)    
+            if el in skip:
+                continue
+            count-=1
+        answer+= el
+    return answer
