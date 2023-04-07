e745374c28e5f466314e9c94d0009cd5a834c79b
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-07 14:58:32 +0900

    [update]

--- programmers/07둘만의암호.py
+++ programmers/07둘만의암호.py
@@ -4,8 +4,8 @@ def solution(s, skip, index):
     for el in s:        
         count=index        
         while count>0:          
-            if ord(el)+1>122:                
-                el=chr(ord(el)+1-26)
+            if el=='z':                
+                el='a'
             else:   
                 el=chr(ord(el)+1)    
             if el in skip:
