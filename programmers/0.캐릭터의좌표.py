5fd6a2435e5a468ea45b32e0259355b43509dc9c
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-07 17:04:05 +0900

    [update]

--- programmers/0.캐릭터의좌표.py
+++ programmers/0.캐릭터의좌표.py
@@ -2,10 +2,9 @@
 def solution(keyinput, board):
     key2move={"up":[0,1], "down":[0,-1], "left":[-1,0], "right":[1,0]}
     r,c=0,0
-
     for key in keyinput:
-        dr, dc = key2move[key][0],key2move[key][1]
-        if abs(r+dr)<=board[0]//2 and abs(c+dc)<=board[1]//2:
-            r+=dr
-            c+=dc
+        dr, dc = key2move[key]
+        nr, nc = r + dr, c + dc
+        if abs(nr)<=board[0]//2 and abs(nc)<=board[1]//2:
+            r, c = nr, nc                 
     return [r,c]
