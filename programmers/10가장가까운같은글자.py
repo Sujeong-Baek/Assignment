67b9be3c5b3ab9ba398b2a20c18ac969c494cc4e
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-07 17:37:09 +0900

    [update]

--- programmers/10가장가까운같은글자.py
+++ programmers/10가장가까운같은글자.py
@@ -1,1 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/142086
+def solution(s):
+    els= ''
+    answer=[]
+    for el in s:
+        if not el in els: 
+            els+=el
+            answer.append(-1)
+        else:
+            answer.append(len(els)-els.index(el))
+            els=els.replace(el,' ')
+            els+=el
+    return answer
