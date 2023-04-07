1c46c12ccfc02ccbde0e0e04409baf3cc8ff5363
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-07 17:37:52 +0900

    [update]

--- programmers/10가장가까운같은글자.py
+++ programmers/10가장가까운같은글자.py
@@ -1,13 +1,9 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/142086
 def solution(s):
-    els= ''
     answer=[]
-    for el in s:
-        if not el in els: 
-            els+=el
-            answer.append(-1)
-        else:
-            answer.append(len(els)-els.index(el))
-            els=els.replace(el,' ')
-            els+=el
+    for i, el in enumerate(s,1):
+        if s[:i].count(el)==1: 
+            answer.append(-1)  
+        else:#i의 el을 제외, reverse하여 처음에나온 el과 같은 문자열의index
+            answer.append(s[i-2::-1].index(el)+1)            
     return answer
