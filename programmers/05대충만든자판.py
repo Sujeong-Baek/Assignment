33042ee34cfac550edb28a6fe44149df21bea910
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-08 20:34:26 +0900

    수업완료

--- programmers/05대충만든자판.py
+++ programmers/05대충만든자판.py
@@ -1,34 +1,40 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/160586
-
 def solution(keymap, targets):
-    answer = [0]*len(targets)
-    
-    for i,target in enumerate(targets): 
-        for t in target:#"A"
-            if answer[i]==-1:
-                continue
-            index_list=check_index_list(t, keymap)
-            key_count=check_minindex(t, index_list,keymap)
-            if key_count!=-1:
-                answer[i]+=(key_count+1)
+    answer = []
+    letter2num = create_letter2num(keymap)
+    for target in targets:
+        count=0
+        for t in target: # target: "ABCD"
+            if t in letter2num:
+                count+=letter2num[t]
             else:
-                answer[i]=-1
+                count= -1
+                break
+        answer.append(count)
     return answer
 
-#targets의 문자열을 받아 존재하면 keymap 인덱스
-def check_index_list(t, keymap):
-    index_list=[]
-    for i, key in enumerate(keymap):        
-        if t in key:
-            index_list.append(i)
-    return index_list
-    
-#index_list에서 작은 위치 반환
-#indexlist=> j작은것
-def check_minindex(t, index_list,keymap):
-    j_index=[]
-    if index_list:
-        for index in index_list:
-            j_index.append(keymap[index].index(t))
-        return min(j_index)
-    return -1
+def create_letter2num(keymap): # ["ABACD", "BCEFD"]
+    letter2num = {}
+    for key in keymap: # key: "ABACD"    
+        for i,k in enumerate(key,1):
+            if not k in letter2num:
+                letter2num[k]=i
+            elif letter2num[k] > i:
+                letter2num[k]=i
+    return letter2num
+
+# ["ABACD", "BCEFD"]
+# A -> 1
+# B -> 1
+# C -> 2
+# D -> 5
+# E -> 3
+# F -> 4
+
+# keymap -> letter2num
+# A -> 1
+# B -> 1
+# C -> 2
+# D -> 5
+# E -> 3
+# F -> 4
