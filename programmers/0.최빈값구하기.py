44f30ef71df08bb292863a4bf110e26322ecdb11
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-08 21:01:27 +0900

    수업완료

--- programmers/0.최빈값구하기.py
+++ programmers/0.최빈값구하기.py
@@ -1,16 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120812
-from collections import defaultdict
+from collections import Counter
 
 def solution(array):
-    el2count = defaultdict(int)
-    answer=[]
-    for el in array:        
-        el2count[el]+=1
-    
-    max_count=max(el2count.values())
- 
-    for el in el2count:
-        if el2count[el]==max_count:
-            answer.append(el)
+    if len(array)==1:
+        return array[0]
+    counter = Counter(array)
+    a, b = counter.most_common(2)
+    return a[0] if a[-1]!=b[-1] else -1
 
-    return -1 if len(answer)>1 else answer[0]
+
+counter = Counter([1, 2, 2,2,3, 3, 3, 4])
+print(counter.most_common(2))
