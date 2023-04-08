0c5f45d95e0aeffa16467c99d1b0894625325cd5
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-08 20:15:25 +0900

    수업완료

--- programmers/05.덧칠하기.py
+++ programmers/05.덧칠하기.py
@@ -6,13 +6,23 @@ def solution(n, m, section):
     #숫자가 0이 아닌 곳부터 시작하여 m만큼의 길이를 (0이 아닌 숫자만 0으로 바꾸기)
     #m만큼의 길이는 리스트의 범위를 벗어나면 안됨
     
-    wall=[0]*n  
-    for num in section:
-        wall[num-1]=num        
-    for i, w in enumerate(wall):
-        if w!=0 :
-            if i<=len(wall):                   
-                wall[i:i+m:]=[0]*m
-                answer+=1    
-   
+    max_painted = 0
+    for s in section:
+        if max_painted<s:
+            max_painted=s+m-1
+            answer+=1
     return answer
+
+
+    # 8	4	[2, 3, 6]	2
+    # 1 2 3 4 5 6 7 8
+    #   X X X X
+    #           X X X X 
+    # max_painted = 0
+    # max_painted = 2 + 4 - 1 = 5
+    # max_painted = 6 + 4 - 1 = 9
+
+
+    # i 번째를 돌 때 i-1번까지는 이미 다 칠해졌다고 가정하고
+    # i 번째를 칠하면 i+m-1 번째까지는 칠해진다.
+    # max_painted = i + m - 1
