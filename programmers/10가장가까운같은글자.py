ff283276b1c35943d71c0f538d0e10301bd845db
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-08 21:16:54 +0900

    [update]

--- programmers/10가장가까운같은글자.py
+++ programmers/10가장가까운같은글자.py
@@ -1,9 +1,25 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/142086
 def solution(s):
     answer=[]
-    for i, el in enumerate(s,1):
-        if s[:i].count(el)==1: 
-            answer.append(-1)  
-        else:#i의 el을 제외, reverse하여 처음에나온 el과 같은 문자열의index
-            answer.append(s[i-2::-1].index(el)+1)            
+    letter2idx = {}
+    for idx, letter in enumerate(s):
+        if letter not in letter2idx:
+            letter2idx[letter]==idx
+            answer.append(-1)
+        else:
+            answer.append(idx-letter2idx[letter])
+            letter2idx[letter]==idx
     return answer
+
+# count, index, min(lst), max(lst), find, replace 이런건 안씀
+
+# banana
+# b : -1, b -> 1
+# a : -1, b -> 1, a -> 2
+# n : -1, b -> 1, a -> 2, n -> 3
+# a : 2, b -> 1, a -> 4 - 2 = 2, n -> 3
+# n : 2, b -> 1, a -> 4, n -> 5
+# a : 2, b -> 1, a -> 6, n -> 5
+# banan
+# banana
+# letter2num
