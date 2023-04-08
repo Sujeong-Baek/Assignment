b7fbb2b7f01d29d1ae00a7640776108fbfc37135
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-08 20:46:14 +0900

    수업완료

--- programmers/06카드뭉치.py
+++ programmers/06카드뭉치.py
@@ -1,13 +1,27 @@
+from collections import deque
 # https://school.programmers.co.kr/learn/courses/30/lessons/159994
 def solution(cards1, cards2, goal):
-    
-    for g in goal:
-        if len(cards1)>0 and cards1[0]==g:
-            cards1.pop(0)
-            
-        elif len(cards2)>0 and cards2[0]==g:
-            cards2.pop(0)
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
+    goal = deque(goal)
+    while goal:
+        g = goal.popleft()
+        one = cards1 and cards1[0] == g
+        two = cards2 and cards2[0] == g
+        if one and two:
+            result1 = solution(cards1[1:], cards2, goal)
+            if result1 == "Yes":
+                return "Yes"
+            return solution(cards1, cards2[1:], goal)
+        elif one:
+            cards1.popleft()
+        elif two:
+            cards2.popleft()
         else:
             return "No"
            
     return "Yes"
+
+# lst.pop()
+# lst.append()
+# ["B"],["B", "C"],["B", "C", "B"]
