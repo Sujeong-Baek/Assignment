bacc2a1cacfd5b476d623520e8a4f75322af7735
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-08 20:50:51 +0900

    수업완료

--- programmers/0.컨트롤제트.py
+++ programmers/0.컨트롤제트.py
@@ -1,11 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120853
 
 def solution(s):
-    s_list = s.split()
-    answer=0
-    for i in range(len(s_list)):
-        if s_list[i]=='Z':
-            answer-=int(s_list[i-1])
+    s=s.split(" ")
+    answer=[]
+    for el in s:
+        if el !="Z":
+            answer.append(int(el))
         else:
-            answer+=int(s_list[i])
-    return answer
+            answer.pop()
+    return sum(answer)
+# 1 2 Z 3
+# [1 3]
