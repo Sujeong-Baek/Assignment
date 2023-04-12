cdfaffbad81e40f3c9722acc15eb3dceaef49993
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-12 20:29:11 +0900

    [update]

--- programmers/202과제진행하기.py
+++ programmers/202과제진행하기.py
@@ -1,32 +1,23 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/176962#qna
 def solution(plans):
     answer = []
-    suspended=[]
-    plans=cal_endtime(plans)    
-    end_time=plans[0][2]
-    for i in range(1,len(plans)):
-        if plans[i][1]>=end_time:
-            if not answer:
-                answer.append(plans[i-1][0])
-            answer.append(plans[i][0])
-        elif plans[i][1]<end_time:
-            suspended.append(plans[i-1][0])
-        end_time=plans[i][2]
-    
+    plans=sorted(plans, key=lambda x: x[1])
+    for plan in plans:
+        plan[1]=int(plan[1][:2])*60+int(plan[1][3:])
+        plan[2]=int(plan[2])
+        
+    suspended=[plans[0]]
+    now=plans[0][1]
+    for plan in plans[1:]:
+        while suspended and now+suspended[-1][2]<=plan[1]:
+            now+=suspended[-1][2] 
+            answer.append(suspended.pop()[0])
+        if suspended:
+            suspended[-1][2]-=plan[1]-now
+        suspended.append(plan)
+        now=plan[1]
+
     while suspended:
-        s=suspended.pop()
-        answer.append(s)
-    
+        answer.append(suspended.pop()[0])
+        
     return answer
-
-
-def cal_endtime(plans):
-    plans = sorted(plans, key=lambda x: x[1])
-    for i, (name, start, play_time) in enumerate(plans):
-        start_h, start_m = map(int, start.split(':'))
-        play_time=int(play_time)
-        end_m = (start_m + play_time) % 60
-        end_h = (start_h + (start_m + play_time) // 60) % 24
-        plans[i][1] = start_h * 60 + start_m  # 분 단위로 변경
-        plans[i][2] = end_h * 60 + end_m  # 분 단위로 변경
-    return plans
