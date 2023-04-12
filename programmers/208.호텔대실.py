3089cc5d21df02c6aa5023fd8c81424b3fdc5485
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-12 17:27:47 +0900

    [udpate]분으로 비교

--- programmers/208.호텔대실.py
+++ programmers/208.호텔대실.py
@@ -1,11 +1,12 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/155651
 def solution(book_time):
-    cleaned_time=add_cleaning(book_time)
-    rooms=[cleaned_time[0][1]]
-    for time in cleaned_time[1:]:
+    int_book_time=change_int(book_time)
+    rooms=[int_book_time[0][1]]
+    
+    for time in int_book_time[1:]:
         found_room=False
-        for i,room in enumerate(rooms):            
-            if time[0]>=room:
+        for i,room in enumerate(rooms):
+            if time[0]-room>=10:
                 rooms[i]=time[1]
                 found_room=True
                 break
@@ -14,13 +15,11 @@ def solution(book_time):
 
     return len(rooms)
 
-def add_cleaning(book_time):
+def change_int(book_time):
     book_time=sorted(book_time, key=lambda x: (x[0],x[1]))
-    cleaned_time=[]
+    int_book_time=[]
     for start,end in book_time:
         start_h, start_m = map(int, start.split(":"))
         end_h, end_m = map(int, end.split(":"))
-        end_m+=10
-        end_h+=end_m//60
-        cleaned_time.append([start_h*100+start_m, end_h*100+end_m%60])
-    return cleaned_time
+        int_book_time.append([start_h*60+start_m, end_h*60+end_m])
+    return int_book_time
