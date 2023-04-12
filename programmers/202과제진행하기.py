fe4e56d89bcc2b262f0a8c967cf30735c7dcc714
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-12 20:44:38 +0900

    수업완료

--- programmers/202과제진행하기.py
+++ programmers/202과제진행하기.py
@@ -1,16 +1,12 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/176962#qna
 def solution(plans):
     answer = []
-    plans=sorted(plans, key=lambda x: x[1])
-    for plan in plans:
-        plan[1]=int(plan[1][:2])*60+int(plan[1][3:])
-        plan[2]=int(plan[2])
-        
+    plans = int_plans(plans)
     suspended=[plans[0]]
     now=plans[0][1]
     for plan in plans[1:]:
         while suspended and now+suspended[-1][2]<=plan[1]:
-            now+=suspended[-1][2] 
+            now+=suspended[-1][2]
             answer.append(suspended.pop()[0])
         if suspended:
             suspended[-1][2]-=plan[1]-now
@@ -20,4 +16,12 @@ def solution(plans):
     while suspended:
         answer.append(suspended.pop()[0])
         
+    return answer
+
+# plans의 원소는 [name, start, playtime]의 구조로 이루어져 있습니다.
+def int_plans(plans):
+    answer = []
+    for name, start, playtime in sorted(plans, key=lambda x: x[1]): #  ["music", "12:20", "40"],
+        start_h, start_m=map(int, start.split(":"))
+        answer.append([name,start_h*60+start_m,int(playtime)])
     return answer
