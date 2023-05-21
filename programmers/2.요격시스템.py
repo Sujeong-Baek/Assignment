6a3ccbe5a1eef980b0b1b2fca98fea3e2162f4f8
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-05-21 13:54:59 +0900

    Update

--- programmers/2.요격시스템.py
+++ programmers/2.요격시스템.py
@@ -1,1 +1,12 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/181188
+def solution(targets):
+    targets=sorted(targets)
+    count = 0
+    interception = {'s' : 0, 'e' : 0}
+    for s, e in targets:
+        if interception['e'] <=s:
+            count += 1
+            interception = {'s' : s, 'e' : e}
+        else:
+            interception = {'s' : max(interception['s'], s), 'e' : min(interception['e'], e)}
+    return count
