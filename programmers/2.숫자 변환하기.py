d216d217ab827bc7d96df6d32d02d86d09c9d89c
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-05-23 01:43:14 +0900

    update

--- programmers/2.숫자 변환하기.py
+++ programmers/2.숫자 변환하기.py
@@ -4,26 +4,22 @@ from collections import deque
 def solution(x, y, n):
     
     q=deque([(x, 0)])
-    visited = set()
-    visited.add(x)
+    visited=set([x])
     time = 0
-   
-    while q:
-        num, time = q.popleft()
-        if num == y:
-            return time
-        for sign, number in [("+", n), ("*", 2), ("*", 3)]:
-            if sign == "+":
-                num+=number
-            elif sign == "*":
-                num*=number
-            if num > y :
-                continue
-            if num in visited :
-                continue
-            
-            visited.add(num)
-            q.append((num, time+1))    
-    return -1
 
-print(solution(10, 40, 5))
+    while q:
+        x, time = q.popleft()
+        if x == y:
+            return time
+        for sign, num in [("+", n), ("*", 2), ("*", 3)]:
+            if sign == "+":
+                nx=x+num
+            elif sign == "*":
+                nx=x*num
+            if nx > y :
+                continue
+            if nx in visited:
+                continue
+            visited.add(nx)
+            q.append((nx,time+1))
+    return -1
