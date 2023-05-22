f24575bbfaf8522f31a538981350fc5dce286ab8
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-05-22 23:24:17 +0900

    update

--- programmers/08개인정보수집유효기간.py
+++ programmers/08개인정보수집유효기간.py
@@ -1,24 +1,26 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/150370#
+# https://school.programmers.co.kr/learn/courses/30/lessons/150370
 from collections import defaultdict
 
 def solution(today, terms, privacies):
     answer = [] 
-    ty,tm,td=today.split('.')
-    today=int(ty)*10000+int(tm)*100+int(td)
-    
-    typ2month = defaultdict(int)
-    for term in terms:
-        t,month=term.split()
-        typ2month[t]=int(month)
-        
-    for i, privacy in enumerate(privacies,1):
-        month=typ2month[privacy[-1]]
-        if today >= calculator_date(month, privacy): 
-            answer.append(i)        
-    return answer
+    today_num =  date2num(today)
 
-#약관 끝난 다음날 구하기
-def calculator_date(month, privacy):
-    py,pm,pd=map(int, privacy[:-1].split("."))
-    py+=(pm+month-1)//12      
-    return py*10000+ ((pm+month-1)%12+1)*100+pd
+    term2month = defaultdict(int)
+    for term in terms:
+        t, month = term.split()
+        term2month[t] = int(month)
+        
+    for i, privacy in enumerate(privacies, 1):
+        date, term = privacy.split()
+        month = term2month[term]
+        if today_num >= calculator_date(month, date): 
+            answer.append(i)        
+    return answer if answer else -1
+
+def date2num(date):
+    year, month, day = date.split('.')
+    return (int(year)*12 + int(month))*28 + int(day)
+
+
+def calculator_date(month, date):   
+    return date2num(date) + 28*month
