9ba9e523a8faa2925a43b9d226b8333f57b155aa
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-05-22 16:22:23 +0900

    Update

--- programmers/2.미로탈출.py
+++ programmers/2.미로탈출.py
@@ -1,67 +1,47 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/159993
 from collections import deque
-
-def solution(maps):
-    n, m = len(maps), len(maps[0])
-    sx, sy, ex, ey, lx, ly = -1, -1, -1, -1, -1, -1
-    dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1] 
-    visited_l = [[False] * len(maps[0]) for _ in range(len(maps))]
-    visited_e = [[False] * len(maps[0]) for _ in range(len(maps))]
-    
-
-    for i in range(n):
-        for j in range(m):
+def find_index(maps):
+    for i in range(len(maps)):
+        for j in range(len(maps[0])):
             if maps[i][j] == 'S':
                 sx, sy = i, j
-            elif maps[i][j] == 'E':
-                ex, ey = i, j
             elif maps[i][j] == 'L':
                 lx, ly = i, j
-    
+            elif maps[i][j] == 'E':
+                ex, ey = i, j
+    return sx, sy, lx, ly, ex, ey
 
+def bfs(maps, sx, sy, ex, ey):
+    n, m = len(maps),len(maps[0])
+    dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1] 
     q = deque([(sx, sy, 0)])
-    visited_l[sx][sy] = True
-    lever_time = 0 
-    while q:
-        x, y, time = q.popleft()
-        
-        if x == lx and y == ly:
-            lever_time = time
-        
-        for k in range(4):
-            nx, ny = x + dx[k], y + dy[k]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if visited_l[nx][ny] or maps[nx][ny] == 'X':
-                continue
-            
-            visited_l[nx][ny] = True
-            q.append((nx, ny, time + 1))
-                
-    if not visited_l[lx][ly]:
-        return -1
-    
+    visited = [[False] * len(maps[0]) for _ in range(len(maps))]
+    visited[sx][sy] = True
+    time = 0
 
-    q = deque([(lx, ly, lever_time)])
-    visited_e[lx][ly] = True
-    time = lever_time
     while q:
         x, y, time = q.popleft()
         
         if x == ex and y == ey:
-            break
+            return time
         
-        for k in range(4):
-            nx, ny = x + dx[k], y + dy[k]
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            if visited_e[nx][ny] or maps[nx][ny] == 'X':
+            if visited[nx][ny] or maps[nx][ny] == 'X':
                 continue
             
-            visited_e[nx][ny] = True
+            visited[nx][ny] = True
             q.append((nx, ny, time + 1))
-
-    if not visited_e[ex][ey]:
+                
+    if not visited[ex][ey]:
         return -1
-    
-    return time
+
+def solution(maps):
+    sx, sy, lx, ly, ex, ey = find_index(maps)
+ 
+    lever_time = bfs(maps, sx, sy, lx, ly)
+    exit_time = bfs(maps, lx, ly, ex, ey)
+
+    return lever_time + exit_time if lever_time != -1 and exit_time != -1 else -1
