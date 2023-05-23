bbefea53ffe3aa8da42a2422a5bcb541129a9054
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-05-24 00:24:07 +0900

    update 1.defaultdict 2.True=1, False=0 ->sum

--- programmers/kakao blind recruitment/2022.신고결과받기.py
+++ programmers/kakao blind recruitment/2022.신고결과받기.py
@@ -1,22 +1,19 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/92334
+from collections import defaultdict
 def solution(id_list, report, k):
-    answer = [0 for _ in range(len(id_list))]
-    reported2user={}
-    mail_id=[]
+    answer = []
+    reported2cnt=defaultdict(int)
+    user2reported=defaultdict(set)
     
     for r in report:
         user, reported =r.split()
-        if reported in reported2user:
-            reported2user[reported].add(user)                  
-        else:
-            reported2user[reported]=set()
-            reported2user[reported].add(user)
+        if reported in user2reported[user]:
+            continue
+        user2reported[user].add(reported)
+        reported2cnt[reported]+=1
 
-    for ide in id_list:
-        if ide in reported2user and len(reported2user[ide])>=k:
-            mail_id.extend(list(reported2user[ide]))
-
-    for idx, ide in enumerate(id_list):     
-        answer[idx]= mail_id.count(ide)
+    for user in id_list:
+        mail_cnt=sum([reported2cnt[reported]>=k for reported in user2reported[user]])
+        answer.append(mail_cnt)
             
     return answer
