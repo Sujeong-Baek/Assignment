f6e1f3e4612e7a058150770efe648bfa013972a7
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-01 00:37:59 +0900

    update

--- programmers/2.디펜스 게임.py
+++ programmers/2.디펜스 게임.py
@@ -1,1 +1,19 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/142085
+from heapq import heappop, heappush
+def solution(n, k, enemy):
+    answer = 0
+    hq=[]
+    for e in enemy:
+        if k>0:
+            heappush(hq, e)
+            k-=1
+            answer+=1
+        else:
+            a=heappop(hq)
+            if n-a >=0:
+                n-=a
+                heappush(hq,e)
+                answer+=1
+            else:
+                break
+    return answer
