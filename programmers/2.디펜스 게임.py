29cfcedab61cf84314f125fadcc2bd78cbc4f0e2
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-03 20:50:40 +0900

    update

--- programmers/2.디펜스 게임.py
+++ programmers/2.디펜스 게임.py
@@ -1,21 +1,19 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/142085
-from heapq import heappop, heappush
+from heapq import heapify, heappop, heappush
 def solution(n, k, enemy):
-    answer = 0
-    hq=[]
-    for e in enemy:
-        if k>0:
-            heappush(hq, e)
-            k-=1
-        else:
-            min_e=heappop(hq)
-            if e<min_e and n-e>=0:
-                heappush(hq, min_e)
-                n-=e
-            elif e>=min_e and n-min_e>=0:
-                heappush(hq, e)
-                n-=min_e                   
-            else:
-                break
+    answer = min(k, len(enemy))
+    hq = enemy[:k]
+    heapify(hq)
+    for e in enemy[k:]:
+        min_e=heappop(hq)
+        n-=min(min_e, e)       
+        if n < 0:
+            break
+        heappush(hq, max(min_e, e))    
         answer+=1
     return answer
+
+lst = [1,2,3,4]
+
+# lst[100]
+# lst[:100]
